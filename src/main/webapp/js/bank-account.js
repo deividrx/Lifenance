d17591b31e866c7cccebaa886c25e331fa944cf8
henriquelsx <henriquelsx@hotmail.com>
2022-06-20 19:22:17 -0300

    bugs fixed buttons

--- src/main/webapp/js/bank-account.js
+++ src/main/webapp/js/bank-account.js
@@ -14,7 +14,7 @@ function populateTable() {
                         $('#tbody').append(
                                 '<tr><td>'
                                         +'<div class="row">'
-                                        + '<div class="col-sm-3">'
+                                        + '<div class="col-">'
                                         + '<form class="delete-conta" method="post" action="controller/bank-account/delete">' 
                                             + '<button class="btn btn-primary"><i class="fas fa-trash"></i></button>'    
                                             + '<input type="hidden" name="id" value="' + data[i].id + '">'
